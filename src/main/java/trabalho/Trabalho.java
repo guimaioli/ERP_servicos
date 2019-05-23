b001dda23514d7e8803b84993eb4cb64929bf5d4
Gabriel Oliveira dos Santos <oliveirasgah@gmail.com>
2019-05-22 22:55:28 -0300

    Troca versão de target do Maven e adiciona tema Nimbus

--- src/main/java/trabalho/Trabalho.java
+++ src/main/java/trabalho/Trabalho.java
@@ -5,6 +5,10 @@
  */
 package trabalho;
 
+import Telas.Login;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+
 /**
  *
  * @author guilherme
@@ -15,7 +19,19 @@ public class Trabalho {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
